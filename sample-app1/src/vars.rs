a1fe6b7153cd2e132cd0a2990c61fa2f6fb3c82a
nagi125 <nagi125.dev@gmail.com>
2021-12-04 14:33:50 +0900

    add: mutable・immutableサンプル追加

--- sample-app1/src/vars.rs
+++ sample-app1/src/vars.rs
@@ -1,8 +1,49 @@
 pub mod sub_a;
 pub mod sub_b;
 
+const MAX_POINTS: u32 = 100_000;
+
 pub fn run() {
     println!("Here is vars module!!");
-    sub_a::func_a();
-    sub_b::func_b();
+    // sub_a::func_a();
+    // sub_b::func_b();
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // 使っていないと明示できる
+    let _i1 = 3;
+    let _f1 = 0.1;
+
+    println!("{}", usize::BITS);
+    println!("Memory address of const is: {:p}", &MAX_POINTS);
+
+    // アドレス確認用
+    let i2: i64 = 1;
+    let i3: i64 = 2;
+
+    // 8byteぶんずれてる
+    println!("Stack address of i2 is: {:p}", &i2);
+    println!("Stack address of i3 is: {:p}", &i3);
+
+    // 新しくStackされていく
+    let y = 5;
+    println!("Stack address of y is: {:p}", &y);
+
+    let y = y + 1;
+    println!("Stack address of y is: {:p}", &y);
+
+    let y = y * 2;
+    println!("Stack address of y is: {:p}", &y);
+    println!("The value of y is: {}", y);
+
+    // ブロック内は別の変数として扱われる
+    {
+        let y = 0;
+        println!("The value of y is: {}", y);
+    }
+
+    println!("The value of y is: {}", y);
 }
